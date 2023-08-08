7cdc1434ed65adf69b172cb46684a1641c0dbbc6
Mr Juice <chigonyetawanda@gmail.com>
2023-08-08 19:12:44 +0200

    Add files via upload

--- plugins/creator.js
+++ plugins/creator.js
@@ -5,8 +5,8 @@ let pp = await conn.profilePictureUrl(who).catch(_ => thumbnailUrl.getRandom())
 let name = await conn.getName(who)
 
   const sentMsg = await conn.sendContactArray(m.chat, [
-    [nomorown, `${await conn.getName(nomorown + '@s.whatsapp.net')}`, `💌 Developer Bot `, `Not Famous`, `sousakura666@gmail.com`, `🇯🇵 japan`, `📍 https://xxirfanx.github.io`, `👤 Owner zoro Bot`],
-    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🎈 Whatsapp Bot`, `📵 Dont Spam`, `Nothing`, `🇯🇵 japan`, `📍 https://github.com/xxirfanx/zoromd`, `Just a normal bot that sometimes has an error ☺`]
+    [nomorown, `${await conn.getName(nomorown + '@s.whatsapp.net')}`, `💌 Developer Bot `, `Not Famous`, `mrjuice017@gmail.com`, `🇺🇲 New York`, `🐦 https://www.youtube.com/@mrjuiceofc`, `👤 Owner BayMax Bot`],
+    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🎈 Whatsapp Bot`, `📵 Dont Spam`, `Nothing`, `🇺🇲 N.Y`, `🐦 https://github.com/mrjuice01/`, `Just a normal bot that sometimes has an error ☺`]
   ], fkontak)
   await conn.reply(m.chat,`Hello @${m.sender.split(`@`)[0]} Thats my owner, dont spam or i will block u`, sentMsg, {
                 mentions: [m.sender]
